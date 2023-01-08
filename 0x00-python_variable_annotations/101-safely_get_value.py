b52036621144db96a7be1c0ad2bd1cdd0addcf9b
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-01-08 14:11:10 +0100

    pycodestyle errors

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -8,7 +8,10 @@ from typing import TypeVar, Mapping, Any, Union
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None)  -> Union[Any, T]:
+def safely_get_value(
+  dct: Mapping,
+  key: Any,
+  default: Union[T, None] = None) -> Union[Any, T]:
     """yet more typing"""
     if key in dct:
         return dct[key]
