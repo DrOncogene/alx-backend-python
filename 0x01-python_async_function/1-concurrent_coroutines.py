be1440b27b6b088f84b18c9801afde24089ccd9d
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-01-10 02:59:52 +0100

    improved function docs and task 2 to measure runtime

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""asyncio syntax"""
+"""chaining in asyncio"""
 import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
@@ -13,7 +13,10 @@ async def get_results(res_arr: List[float], max_delay: int) -> List[float]:
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """runs a function async"""
+    """
+    gathers results executing
+    wait_random n times
+    """
     res = []
     await asyncio.gather(*(get_results(res, max_delay) for _ in range(n)))
 
