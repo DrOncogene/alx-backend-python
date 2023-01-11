8560e8a30d9e3c88a82a3b8873db01263c872d40
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-01-11 01:19:49 +0100

    changed the random lower limit

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,9 +7,9 @@ import random
 async def async_generator():
     """
     yields a random float between
-    1 and 10 ten times
+    0 and 10 ten times
     """
     for _ in range(10):
         await asyncio.sleep(1)
-        num = random.uniform(1, 10)
+        num = random.uniform(0, 10)
         yield num
