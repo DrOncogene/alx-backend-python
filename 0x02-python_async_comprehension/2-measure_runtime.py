a34b0a549920563d96234212fc0f9c006e631d5d
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-01-11 01:32:26 +0100

    measure performance of async comprehensions

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+measuring performance
+"""
+import time
+import asyncio
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    measures the perf of async
+    comprehensions
+    """
+    start = time.perf_counter()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end = time.perf_counter()
+
+    return end - start
