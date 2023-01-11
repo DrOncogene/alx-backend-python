684da834556c306fffbfb97da9ec7ba19c5ae664
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-01-11 01:38:42 +0100

    added correct generator annotations

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """async generator"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     yields a random float between
     0 and 10 ten times
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,9 +2,10 @@
 """
 async comprehension
 """
+from typing import Generator
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list:
+async def async_comprehension() -> Generator[list, None, None]:
     """loops over async generator 10 times"""
     return [num async for num in async_generator()]
