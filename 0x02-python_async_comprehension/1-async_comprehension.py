f184eae7008732963dba4b3aac8682127008cce1
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-01-11 01:41:11 +0100

    pppfffffttttt

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,10 +2,9 @@
 """
 async comprehension
 """
-from typing import Generator
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[list, None, None]:
+async def async_comprehension() -> list:
     """loops over async generator 10 times"""
     return [num async for num in async_generator()]
