3330634344fb26de1405744e89660abc57976577
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-01-11 14:11:36 +0100

    used the typing module for annotation

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -6,6 +6,6 @@ from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List:
+async def async_comprehension() -> List[float]:
     """loops over async generator 10 times"""
     return [num async for num in async_generator()]
